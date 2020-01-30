7c9bc9d909a4eab34d7a5c8f3f64c963785192b1
최정균 <wjdrbs966@naver.com>
2020-01-31 03:20:04 +0900

    문자열을 정수로 바꾸기

---
+++ StringChange.java
@@ -0,0 +1,17 @@
+package Programmers;
+
+import java.util.Scanner;
+
+public class StringChange {
+    public static int solution(String s) {
+        int answer = 0;
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String s = input.next();
+
+        System.out.println(solution(s));
+    }
+}
