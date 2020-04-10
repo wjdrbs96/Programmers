dcd7ee35e3eab62d982725dc749dcd11eb905a47
최정균 <wjdrbs966@naver.com>
2020-04-11 01:41:52 +0900

    Programmers Level2 Sovled

---
+++ Algorithm/Level2/Top.java
@@ -0,0 +1,25 @@
+package Programmers.Algorithm.Level2;
+
+public class Top {
+    public static int[] solution(int[] heights) {
+        int[] answer = new int[heights.length];
+
+        for (int i = 1; i < heights.length; ++i) {
+            for (int j = i - 1; j >= 0; --j) {
+                if (heights[i] < heights[j]) {
+                    answer[i] = j + 1;
+                    break;
+                }
+            }
+        }
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        //int[] heights = {6, 9, 5, 7, 4};
+        int[] heights = {3,9,9,3,5,7,2};
+        solution(heights);
+    }
+}
