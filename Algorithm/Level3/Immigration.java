64b68b6672dbc7b4d26db56ca9d5b861024cb75a
wjdrbs96 <wjdrbs966@naver.com>
2021-03-31 00:23:10 +0900

    Programmers Level3 입국심사 update

--- Algorithm/Level3/Immigration.java
+++ Algorithm/Level3/Immigration.java
@@ -1,39 +1,44 @@
 package Programmers.Algorithm.Level3;
 
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public long solution(int n, int[] times) {
+        long answer = 0;
+
         Arrays.sort(times);
-        return binerySearch(times, n, times[times.length - 1]);
-    }
+        long start, mid, end;
+        start = 0;
+        end = Long.MAX_VALUE;
+        long sum;
 
-    long binerySearch(int[] times, int n, long max){
-        long left = 1, right = max * n;
-        long mid = 0;
-        long ans = Long.MAX_VALUE;
+        while (start <= end){
 
-        while(left <= right){
-            mid = (left + right) / 2;
+            mid = (start + end) / 2;
+            sum = 0;
 
-            if(isPassed(times, n, mid)){
-                ans = ans > mid ? mid : ans;
-                right = mid - 1;
-            } else {
-                left = mid + 1;
+            // times = [7, 10], n = 6    최대걸리는 시간 60
+            // 모든 입국 심사관들이 총 받을 수 있는 사람 수
+            for (int i = 0; i < times.length; i++) {
+                sum += mid / times[i];   // 30 / 7 => 4명
+                // n => 인원 수
+                if (sum >= n) {
+                    break;
+                }
+            }
+
+            // 중간 시간 모든 인원을 심사 못했을 때
+            if (n > sum) {
+                start = mid + 1;
+            }
+
+            // 중간 시간 모든 인원을 심사 했을 때
+            else {
+                end = mid - 1;
+                answer = mid;
             }
         }
-        return ans;
-    }
 
-    boolean isPassed(int[] times, int n, long mid){
-        long amount = 0;
-
-        for(int i = 0 ; i < times.length ; ++i){
-            amount += mid / times[i];
-        }
-
-        if(amount >= n) return true;
-        else return false;
+        return answer;
     }
 }
